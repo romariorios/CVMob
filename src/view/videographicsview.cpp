f75db03524d17bb08c30aaffe143d7c3fbf7aa8f
Luiz Romário Santana Rios <luizromario@gmail.com>
2012-10-13 11:27:41 -0300

    Attempt to make distance line in creation follow the cursor

--- src/view/videographicsview.cpp
+++ src/view/videographicsview.cpp
@@ -31,7 +31,22 @@ void VideoGraphicsView::mouseMoveEvent(QMouseEvent *event)
 
 const QPointF VideoGraphicsView::mapToPositionInScene(QPointF p) const
 {
-    p.setX(sceneRect().width() * (p.x() / width()));
-    p.setY(sceneRect().height() * (p.y() / height()));
+    // "a" and "b" are dimensions
+    // sceneRect.lengthOfA() / sceneRect.lengthOfB() < lengthOfA() / lengthOfB()
+    // The "dominant" dimension is the dimension which defines the size of the scene
+
+    const bool isWidthDominant = width() / height() > sceneRect().width() / sceneRect().height();
+    const double sa = isWidthDominant? sceneRect().height() : sceneRect().width();
+    const double sb = isWidthDominant? sceneRect().width() : sceneRect().height();
+    const double va = isWidthDominant? height() : width();
+    const double vb = isWidthDominant? width() : height();
+    const double a = isWidthDominant? p.y() : p.x();
+    const double b = isWidthDominant? p.x() : p.y();
+
+    const double as = (a - va / 2) * (sb / vb) + sa / 2;
+    const double bs = b * (sb / vb);
+
+    p.setX(isWidthDominant? bs : as);
+    p.setY(isWidthDominant? as : bs);
     return p;
 }
