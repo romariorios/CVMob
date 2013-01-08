b4694d473957a6a1a96b47caa5eb77af1c19e716
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-01-08 15:01:31 -0300

    Finished trajectories proxy model (not yet tested)

--- src/model/lineartrajectoriesproxymodel.cpp
+++ src/model/lineartrajectoriesproxymodel.cpp
@@ -8,10 +8,44 @@ LinearTrajectoriesProxyModel::LinearTrajectoriesProxyModel(QObject *parent) :
 
 QVariant LinearTrajectoriesProxyModel::data(const QModelIndex &proxyIndex, int role) const
 {
+    if (role != Qt::DisplayRole ||
+        !proxyIndex.isValid()) {
+        return QVariant();
+    }
+
+    if (!proxyIndex.parent().isValid()) {
+        return tr("Trajectory %1").arg(proxyIndex.row());
+    }
+
+    QVariant data = mapToSource(proxyIndex).data(VideoModel::VideoSceneEditRole);
+
+    switch (proxyIndex.column()) {
+    case VideoModel::LFrameColumn:
+        return data;
+    case VideoModel::PositionColumn:
+        return tr("(%1, %2)").arg(data.toPointF().x(), data.toPointF().y());
+    case VideoModel::LSpeedColumn:
+    case VideoModel::LAccelerationColumn:
+        return QLineF(QPointF(0, 0), data.toPointF()).length();
+    default:
+        return QVariant();
+    }
 }
 
 QVariant LinearTrajectoriesProxyModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
+    switch (section) {
+    case VideoModel::LFrameColumn:
+        return tr("Frame");
+    case VideoModel::PositionColumn:
+        return tr("Position");
+    case VideoModel::LSpeedColumn:
+        return tr("Speed");
+    case VideoModel::LAccelerationColumn:
+        return tr("Acceleration");
+    default:
+        return QVariant();
+    }
 }
 
 QModelIndex LinearTrajectoriesProxyModel::mapFromSource(const QModelIndex &sourceIndex) const
@@ -60,8 +94,18 @@ QModelIndex LinearTrajectoriesProxyModel::parent(const QModelIndex &child) const
 
 int LinearTrajectoriesProxyModel::rowCount(const QModelIndex &parent) const
 {
+    if (!parent.isValid()) {
+        return sourceModel()->rowCount(_parentIndex);
+    }
+
+    return sourceModel()->rowCount(mapToSource(parent));
 }
 
 int LinearTrajectoriesProxyModel::columnCount(const QModelIndex &parent) const
 {
+    if (!parent.isValid()) {
+        return 1;
+    }
+
+    return VideoModel::LinearTrajectoryInstantColumnCount;
 }
