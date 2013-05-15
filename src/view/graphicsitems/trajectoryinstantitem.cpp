5f7c65b8d83a3f89d9820e5c6bde38ac67ef5661
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-05-15 09:12:57 +0900

    Initial implementation of TrajectoryInstantItem

---
+++ src/view/graphicsitems/trajectoryinstantitem.cpp
@@ -0,0 +1,114 @@
+#include "trajectoryinstantitem.hpp"
+
+#include <QGraphicsLineItem>
+
+const QPointF radius = QPointF(1, 1);
+
+TrajectoryInstantItem::TrajectoryInstantItem(QPointF pos, QPointF speed, QPointF accel,
+                                             QGraphicsLineItem *lineBefore,
+                                             TrajectoryInstantItem *instantAfter) :
+    _speed(speed),
+    _accel(accel),
+    _speedLine(new QGraphicsLineItem(QLineF(pos, pos + speed), this)),
+    _accelLine(new QGraphicsLineItem(QLineF(pos, pos + accel), this)),
+    _lineBefore(lineBefore),
+    _instantAfter(instantAfter)
+{
+    setRect(QRectF(pos - radius, pos + radius));
+}
+
+TrajectoryInstantItem::~TrajectoryInstantItem()
+{
+    delete _instantAfter->_lineBefore;
+    _instantAfter->_lineBefore = _lineBefore;
+    _lineBefore->setLine(QLineF(_lineBefore->line().p1(), _instantAfter->pos()));
+}
+
+bool TrajectoryInstantItem::isSpeedVisible() const
+{
+    return _speedLine->isVisible();
+}
+
+bool TrajectoryInstantItem::isAccelVisible() const
+{
+    return _accelLine->isVisible();
+}
+
+const QPointF TrajectoryInstantItem::pos() const
+{
+    return rect().topLeft() + radius;
+}
+
+void TrajectoryInstantItem::setSpeedVisible(bool visible)
+{
+    if (visible == isSpeedVisible()) {
+        return;
+    }
+
+    _speedLine->setVisible(visible);
+}
+
+void TrajectoryInstantItem::setAccelVisible(bool visible)
+{
+    if (visible == isAccelVisible()) {
+        return;
+    }
+
+    _accelLine->setVisible(visible);
+}
+
+void TrajectoryInstantItem::setPos(const QPointF &pos)
+{
+    QRectF newRect(pos - radius, pos + radius);
+
+    if (newRect == rect()) {
+        return;
+    }
+
+    setRect(newRect);
+
+    QLineF l;
+    if (_lineBefore) {
+        l = _lineBefore->line();
+        _lineBefore->setLine(QLineF(l.p1(), pos));
+    }
+
+    if (_instantAfter) {
+        l = _instantAfter->_lineBefore->line();
+        _instantAfter->_lineBefore->setLine(QLineF(pos, l.p2()));
+    }
+}
+
+void TrajectoryInstantItem::setSpeed(const QPointF &speed)
+{
+    if (speed == _speed) {
+        return;
+    }
+
+    _speedLine->setLine(QLineF(pos(), speed));
+    _speed = speed;
+}
+
+void TrajectoryInstantItem::setAccel(const QPointF &accel)
+{
+    if (accel == _accel) {
+        return;
+    }
+
+    _accelLine->setLine(QLineF(pos(), accel));
+    _accel = accel;
+}
+
+void TrajectoryInstantItem::setLineBefore(QGraphicsLineItem *lineBefore)
+{
+    if (!_lineBefore) {
+        _lineBefore = lineBefore;
+    }
+}
+
+void TrajectoryInstantItem::setInstantAfter(TrajectoryInstantItem *instantAfter)
+{
+    if (!_instantAfter) {
+        _instantAfter = instantAfter;
+    }
+}
---
+++ src/view/graphicsitems/trajectoryinstantitem.hpp
@@ -0,0 +1,40 @@
+#ifndef TRAJECTORYINSTANTITEM_HPP
+#define TRAJECTORYINSTANTITEM_HPP
+
+#include <QGraphicsRectItem>
+
+class QGraphicsLineItem;
+
+class TrajectoryInstantItem : public QGraphicsRectItem
+{
+public:
+    TrajectoryInstantItem(QPointF pos, QPointF speed, QPointF accel,
+                          QGraphicsLineItem *lineBefore = 0,
+                          TrajectoryInstantItem *instantAfter = 0);
+    ~TrajectoryInstantItem();
+
+    bool isSpeedVisible() const;
+    bool isAccelVisible() const;
+    const QPointF pos() const;
+    inline const QPointF speed() const { return _speed; }
+    inline const QPointF accel() const { return _accel; }
+
+    void setSpeedVisible(bool visible);
+    void setAccelVisible(bool visible);
+    void setPos(const QPointF &pos);
+    void setSpeed(const QPointF &speed);
+    void setAccel(const QPointF &accel);
+    void setLineBefore(QGraphicsLineItem *lineBefore);
+    void setInstantAfter(TrajectoryInstantItem *instantAfter);
+
+private:
+    QPointF _speed;
+    QPointF _accel;
+    QGraphicsLineItem *_speedLine;
+    QGraphicsLineItem *_accelLine;
+
+    QGraphicsLineItem *_lineBefore;
+    TrajectoryInstantItem *_instantAfter;
+};
+
+#endif // TRAJECTORYINSTANTITEM_HPP
