5490fba1aa2ae5aaa19e4b7a28e48b87c2f28299
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-06-10 22:04:02 +0900

    Deleting speed and accel lines at trajectory instant destruction

--- src/view/graphicsitems/trajectoryinstantitem.cpp
+++ src/view/graphicsitems/trajectoryinstantitem.cpp
@@ -27,6 +27,9 @@ TrajectoryInstantItem::~TrajectoryInstantItem()
     delete _instantAfter->_lineBefore;
     _instantAfter->_lineBefore = _lineBefore;
     _lineBefore->setLine(QLineF(_lineBefore->line().p1(), _instantAfter->pos()));
+
+    delete _speedLine;
+    delete _accelLine;
 }
 
 bool TrajectoryInstantItem::isSpeedVisible() const
