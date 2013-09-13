aeb2a53f0087a7473bd890f5f40c330718936744
Luiz Romário Santana Rios <luizromario@gmail.com>
2013-09-12 21:50:57 -0300

    Initial AngleItem implementation

---
+++ src/view/graphicsitems/angleitem.cpp
@@ -0,0 +1,68 @@
+/*
+    CVMob - Motion capture program
+    Copyright (C) 2013  The CVMob contributors
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "angleitem.hpp"
+
+#include <QGraphicsLineItem>
+#include <QGraphicsRectItem>
+#include <QPen>
+
+AngleItem::AngleItem(QGraphicsItem* parent) :
+    _center(new QGraphicsRectItem(this)),
+    _edge1(new QGraphicsRectItem(this)),
+    _edge2(new QGraphicsRectItem(this)),
+    _line1(new QGraphicsLineItem(this)),
+    _line2(new QGraphicsLineItem(this))
+{
+    addToGroup(_line1);
+    addToGroup(_line2);
+    addToGroup(_center);
+    addToGroup(_edge1);
+    addToGroup(_edge2);
+    
+    _edge1->setPen(Qt::NoPen);
+    _edge1->setBrush(Qt::red);
+    
+    _edge2->setPen(Qt::NoPen);
+    _edge2->setBrush(Qt::red);
+    
+    _center->setPen(Qt::NoPen);
+    _center->setBrush(Qt::red);
+}
+
+AngleItem::AngleItem(const QPointF& center, const QPointF& edge1, const QPointF& edge2, QGraphicsItem* parent) :
+    AngleItem(parent)
+{
+    setPoints(center, edge1, edge2);
+}
+
+QRectF rectFromPoint(const QPointF &point)
+{
+    return QRectF(point - QPointF(2, 2), QSizeF(4, 4));
+}
+
+void AngleItem::setPoints(const QPointF& center, const QPointF& edge1, const QPointF& edge2)
+{
+    _center->setRect(rectFromPoint(center));
+    _edge1->setRect(rectFromPoint(edge1));
+    _edge2->setRect(rectFromPoint(edge2));
+    
+    _line1->setLine(QLineF(center, edge1));
+    _line2->setLine(QLineF(center, edge2));
+}
---
+++ src/view/graphicsitems/angleitem.hpp
@@ -0,0 +1,44 @@
+/*
+    CVMob - Motion capture program
+    Copyright (C) 2013  The CVMob contributors
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef ANGLEITEM_HPP
+#define ANGLEITEM_HPP
+
+#include <QGraphicsItemGroup>
+
+class QGraphicsLineItem;
+class QGraphicsRectItem;
+
+class AngleItem : public QGraphicsItemGroup
+{
+public:
+    explicit AngleItem(QGraphicsItem* parent = 0);
+    AngleItem(const QPointF &center, const QPointF &edge1, const QPointF &edge2, QGraphicsItem* parent = 0);
+    
+    void setPoints(const QPointF &center, const QPointF &edge1, const QPointF &edge2);
+    
+private:
+    QGraphicsRectItem *_center;
+    QGraphicsRectItem *_edge1;
+    QGraphicsRectItem *_edge2;
+    QGraphicsLineItem *_line1;
+    QGraphicsLineItem *_line2;
+};
+
+#endif
